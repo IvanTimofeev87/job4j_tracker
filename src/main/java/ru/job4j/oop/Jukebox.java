45be62ec86db013f844257998df46264f2c97049
IvanTimofeev87 <ivant87@mail.ru>
2021-06-30 16:25:04 +0300

    Created class Jukebox

---
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -0,0 +1,20 @@
+package ru.job4j.oop;
+
+public class Jukebox {
+    public void music(int position) {
+        if (position == 1) {
+            System.out.println("Пусть бегут неуклюже\n" + "Пешеходы по лужам.");
+        } else if (position == 2) {
+            System.out.println("Спят усталые игрушки, книжки спят.\n" + "Одеяла и подушки ждут ребят.");
+        } else {
+            System.out.println("Песня не найдена");
+        }
+    }
+
+    public static void main(String[] args) {
+        Jukebox musicBox = new Jukebox();
+        musicBox.music(1);
+        musicBox.music(2);
+        musicBox.music(3);
+    }
+}
