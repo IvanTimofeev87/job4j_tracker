7d4da563fd245c3a905193544d558e33702529e3
IvanTimofeev87 <ivant87@mail.ru>
2021-08-05 23:34:29 +0300

    Modified class ru.job4j.oop.Max

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -2,12 +2,21 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        int result = left > right ? left : right;
-        return result;
+       return left > right ? left : right;
+    }
+
+    public static int max(int first, int second, int third) {
+        return first > Max.max(second, third) ? first : Max.max(second, third);
+    }
+
+    public static int max(int first, int second, int third, int fourth) {
+        return Max.max(first, second) > Max.max(fourth, third) ? Max.max(first, second) : Max.max(fourth, third);
     }
 
     public static void main(String[] args) {
-        int biggestNumber = Max.max(6, 2);
-        System.out.println(" First number 6, second number 2. Biggest number is " + biggestNumber);
+        int biggestNumberFromThree = Max.max(6, 2, 7);
+        System.out.println(" First number 6, second number 2, third number 7. Biggest number is " + biggestNumberFromThree);
+        int biggestNumberFromFour = Max.max(6, 2, 7, 9);
+        System.out.println(" First number 6, second number 2, third number 7, fourth number 9. Biggest number is " + biggestNumberFromFour);
     }
 }
