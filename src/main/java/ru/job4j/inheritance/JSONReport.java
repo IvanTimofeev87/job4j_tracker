46b9e7c574ba311d2d62f093b07eafd6d9587fe1
IvanTimofeev87 <ivant87@mail.ru>
2021-09-14 15:01:04 +0300

    Fixed task overriding

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -1,10 +1,16 @@
 package ru.job4j.inheritance;
 
+import ru.job4j.tracker.Item;
+
+import java.time.format.DateTimeFormatter;
+
 public class JSONReport extends TextReport {
 
     @Override
     public String generate(String name, String body) {
-        return "name : " + name + "," + System.lineSeparator() + "body : " + body;
+        return "{" + System.lineSeparator() + "\"name\" : " + "\"" + name + "\","
+                + System.lineSeparator()
+                + "\"body\" : " + "\"" + body + "\"," + System.lineSeparator() + "}";
     }
 
 }
