449b41609dae7b8973a5ca0cb58dac6b48f1746e
IvanTimofeev87 <ivant87@mail.ru>
2022-04-03 16:06:49 +0300

    Modified class ru.job4j.io.Matches

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -12,7 +12,7 @@ public class Matches {
             String player = turn ? "Первый игрок" : "Второй игрок";
             System.out.println(player + " введите число от 1 до 3:");
             int matches = Integer.parseInt(input.nextLine());
-            while (matches > 3 || matches > count) {
+            while (matches > 3 || matches > count || matches < 1) {
                 System.out.println("Введено неверное значение. Повторите ввод.");
                 matches = Integer.parseInt(input.nextLine());
             }
