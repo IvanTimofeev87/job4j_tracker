900a6d548ffc9613b6c7fad6d75c0f0cba61023d
IvanTimofeev87 <ivant87@mail.ru>
2022-09-17 18:01:07 +0300

    3. Интерфейс Input

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void howDrive() {
+
+    }
+
+    @Override
+    public void numbersOfPassengers(int numbers) {
+
+    }
+
+    @Override
+    public double numberForRefueling(double fuel) {
+        return 0;
+    }
+}
---
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,9 @@
+package ru.job4j.poly;
+
+public interface Transport {
+    void howDrive();
+
+    void numbersOfPassengers(int numbers);
+
+    double numberForRefueling(double fuel);
+}
